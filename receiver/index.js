f6f983fe6557494965a807af3826e0f9447285ee
matheuspavin <pavinmatheus@gmail.com>
2018-02-09 19:04:04 -0200

    Adjusting the tests

--- receiver/index.js
+++ receiver/index.js
@@ -1,25 +1,21 @@
 'use strict'
 const net = require('net');
 
-const PORT = 8080;
-const ADDRESS = 'localhost';
+const port = 8080;
+const adress = 'localhost';
 
 // let the listener, for the system kill the process
 process.on('SIGTERM', function() {
   process.exit(0)
 })
 
-
 // create a base socket, listening to the data transfers...
 // ...didn't create on.end, because, after receive the data, i can kill the process.
-net.createServer(socket => {
-    var start = process.hrtime();
+module.exports = net.createServer(socket => {
     socket.on('data', function(data) {
         // I have to do the two process, because the data came without quotes,... 
         //...so i can't think in a best way, without use a non-native library.
         console.log(JSON.stringify(JSON.parse(data)));
-        var elapsed = process.hrtime(start)[1] / 1000000;
-        console.error('latency : ' + (process.hrtime(start)[0] + " s, " + elapsed.toFixed(3)));
     });
-    socket.on('end', () =>  socket.destroy());
-}).listen(PORT, ADDRESS);
+    socket.on('end', () =>  { return socket.destroy()});
+}).listen(port, adress);
--- tests.js
+++ tests.js
@@ -1,11 +1,20 @@
 const transmitService = require('./sender/transmit.js');
+const receiverService = require('./receiver/index.js');
 var expect = require('chai').expect
+var net = require('net');
+
+
+var host = '127.0.0.1';
+var port = 8080;
+var client = new net.Socket();
+receiverService;
+const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in placerat libero...'
 
 beforeEach(async function () {
 });
 
 describe('Must make a connection and send a message to the server. (Client Tests)',  function () {
-    let message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in placerat libero...'
+    
     it('Get an error with an empty message', async function () {
         let connection = await transmitService('', 'utf8', '');
         expect(connection.toString()).to.be.equal('Error: The server did not accept empty strings');
@@ -17,8 +26,21 @@ describe('Must make a connection and send a message to the server. (Client Tests
     it('Get an ok message', async function () {
         let callback =  function () {return 'OK'};
         let connection = await transmitService(message, 'utf8', callback);
-        console.log(connection);
-        // expect(connection.toString()).to.be.equal('Error: The server did not accept empty encoding');
+        expect(!connection);
     });
-    
+});
+
+describe('Must check the server. (Server Tests)',  function () {
+    it('Check the amount of active connections with no client connected', async function () {
+        expect(receiverService._connections).to.be.equal(0);
+    });
+    it('Check the amount of active connections', async function () {
+        let result;
+        client.connect(port, host, function() {
+            result = client.write(JSON.stringify(message));
+            expect(receiverService._connections).to.be.equal(1);
+            client.destroy();
+        });
+        
+    });
 });
